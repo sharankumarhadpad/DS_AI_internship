d94af867bd05beda9ea22187722519c2897e06c6
k sharankumar <sharankumarhadpad6@gmail.com>
2026-02-03 12:17:41 +0530

    2nd day task

---
+++ sources/day2.py
@@ -0,0 +1,17 @@
+name = input("Enter your name")
+age = int(input("Enter your current age"))
+age_in_2030 = age + 4
+print(f"\n hey{name}, you will be {age_in_2030},years old in 2030!")
+
+# Bill spliter
+total_bill = float(input("Enter your bill"))
+Number_of_people = int(input())
+Share_per_person = total_bill/Number_of_people
+print ("\n",Share_per_person)
+
+# Understanding Data Types and String Formatting.
+item_name = ("\n Laptop")
+quantity = 2
+price = 499.99
+n_stock = True
+print(f"\n items {item_name}, Qty {quantity}, Price{print}, Available{n_stock}")
