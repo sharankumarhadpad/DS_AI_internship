bbcd3cee48a764043cddc2820428334b40e159d0
k sharankumar <sharankumarhadpad6@gmail.com>
2026-02-04 12:53:52 +0530

    day3task

---
+++ sources/day3task.py
@@ -0,0 +1,20 @@
+# the inventory manager
+inventory = [" Apples","Carrot","Bananas","Dates"]
+print(inventory.append("eggs"),inventory)
+print(inventory.remove("Bananas"),inventory)
+print(inventory[0:4])
+inventory.sort()  
+print(inventory) 
+
+ # the data slicer
+temperature = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print(temperature[0],temperature[-1])
+print(temperature[3:6])
+print(temperature[-3:])
+
+# the immutable config
+screen_res = (1920, 1080)
+print("the resolution is",screen_res)
+print("tuples cannot be modified")
+
+
